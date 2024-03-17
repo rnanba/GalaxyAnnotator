f5ea864e5c9f2f57aec329c1c44e37d0a6412a62
R.Nanba <rnanba@gmail.com>
2024-03-17 10:12:52 +0900

    leda-get-votable.py のクエリーの修正、オプションの追加
    - '-m MAG' オプションで指定した等級またはそれより明るい銀河のデータの
      みを取得できるようにした。
    - 取得データに別名(メシエ番号等)を含めるようにした。

--- leda-get-votable.py
+++ leda-get-votable.py
@@ -10,6 +10,8 @@ argparser.add_argument('wcs_fits', metavar='input_wcs.fits',
 argparser.add_argument('votable_xml', metavar='output_votable.xml', nargs='?',
                        help="output file (data of galaxies from HyperLeda in '\
                        'VOTABLE format). if omitted, output to stndard output.")
+argparser.add_argument("-m", "--max-magnitude", dest="max_mag", type=float,
+                       help="maximum total magnitude(it,vt,bt)", metavar="MAG")
 argparser.add_argument("-f", "--force-overwrite", action="store_true",
                        help="force overwriting to output file.")
 args = argparser.parse_args()
@@ -24,7 +26,7 @@ if args.votable_xml:
         if input.upper() != 'YES':
             print('bye.')
             sys.exit()
-
+    
 import json
 import urllib.request
 from astropy import units as u
@@ -51,8 +53,16 @@ dec_min = min(sky_top_left.dec.degree, sky_top_right.dec.degree,
 dec_max = max(sky_top_left.dec.degree, sky_top_right.dec.degree,
               sky_bottom_left.dec.degree, sky_bottom_right.dec.degree)
 
-where = "al2000<{ra_max} and al2000>{ra_min} and de2000<{dec_max} and de2000>{dec_min} and objtype='G'".format(ra_min=ra_min, ra_max=ra_max, dec_min=dec_min, dec_max=dec_max)
+mag_where = ""
+if args.max_mag:
+    mm = args.max_mag
+    mag_where = f"and (it<={mm} or vt<={mm} or it<={mm})"
+
+where = f"al2000<{ra_max} and al2000>{ra_min} and de2000<{dec_max} and de2000>{dec_min} and objtype='G' {mag_where}"
 #print(where)
+
+select = "pgc,objname,objtype,al1950,de1950,al2000,de2000,l2,b2,sgl,sgb,f_astrom,type,bar,ring,multiple,compactness,t,e_t,agnclass,logd25,e_logd25,logr25,e_logr25,pa,brief,e_brief,ut,e_ut,bt,e_bt,vt,e_vt,it,e_it,kt,e_kt,m21,e_m21,mfir,ube,bve,vmaxg,e_vmaxg,vmaxs,e_vmaxs,vdis,e_vdis,vrad,e_vrad,vopt,e_vopt,v,e_v,ag,ai,incl,a21,logdc,btc,itc,ubtc,bvtc,bri25,vrot,e_vrot,mg2,e_mg2,m21c,hic,vlg,vgsr,vvir,v3k,modz,e_modz,mod0,e_mod0,modbest,e_modbest,mabs,e_mabs,hl_names(pgc)"
+
 # leda = 'http://leda.univ-lyon1.fr/fG.cgi'
 leda = 'http://atlas.obs-hp.fr/hyperleda/fG.cgi' # mirror
 # http://atlas.obs-hp.fr/hyperleda/fG.cgi?n=meandata&c=o&of=1,leda,simbad&nra=l&nakd=1&sql=al2000%3C12.965687916818412%20and%20al2000%3E12.925228843825312%20and%20de2000%3C21.871941710192353%20and%20de2000%3E21.491211008587133%20and%20objtype%3D%27G%27&ob=&a=x
@@ -62,6 +72,7 @@ query = urllib.parse.urlencode({
     'of': '1,leda,simbad',
     'nra': 'l',
     'nakd': '1',
+    'd': select,
     'sql': where,
     'ob': '',
     'a': 'x'
